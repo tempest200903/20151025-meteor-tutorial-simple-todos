70f03e1c661cb8b97891f106dabbe5cf8259ca42
Your Name <you@example.com>
2015-11-08 03:33:50 -0500

    tutorial 1 Creating your first app

--- README.md
+++
@@ -1,2 +0,0 @@
-# 20151025-meteor-tutorial-simple-todos
-see https://www.meteor.com/tutorials/blaze/creating-an-app
--- simple-todos.js
+++ simple-todos.js
@@ -1,61 +1,23 @@
-Tasks = new Mongo.Collection("tasks");
-
 if (Meteor.isClient) {
-    // This code only runs on the client
-    Template.body.helpers({
-	tasks: function () {
-	    if (Session.get("hideCompleted")) {
-		// If hide completed is checked, filter tasks
-		return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-	    } else {
-		// Otherwise, return all of the tasks
-		return Tasks.find({}, {sort: {createdAt: -1}});
-	    }
-	},
-	hideCompleted: function () {
-	    return Session.get("hideCompleted");
-	},
-	incompleteCount: function () {
-	    return Tasks.find({checked: {$ne: true}}).count();
-	}
-    });
-    Template.body.events({
-	"submit .new-task": function (event) {
-	    // Prevent default browser form submit
-	    event.preventDefault();
+  // counter starts at 0
+  Session.setDefault('counter', 0);
 
-	    // Get value from form element
-	    var text = event.target.text.value;
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
 
-	    // Insert a task into the collection
-	    Tasks.insert({
-		text: text,
-		createdAt: new Date(), // current time
-		owner: Meteor.userId(),           // _id of logged in user
-		username: Meteor.user().username  // username of logged in user
-	    });
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
 
-	    // Clear form
-	    event.target.text.value = "";
-	},
-	"change .hide-completed input": function (event) {
-	    Session.set("hideCompleted", event.target.checked);
-	}
-    });
-
-    Template.task.events({
-	"click .toggle-checked": function () {
-	    // Set the checked property to the opposite of its current value
-	    Tasks.update(this._id, {
-		$set: {checked: ! this.checked}
-	    });
-	},
-	"click .delete": function () {
-	    Tasks.remove(this._id);
-	}
-    });
-    Accounts.ui.config({
-	passwordSignupFields: "USERNAME_ONLY"
-    });
-
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
 }
